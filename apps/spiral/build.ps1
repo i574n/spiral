a85f5d4c087d4ef77abff43c4f40a63c19b7861b
프리마 <fc1943s@pm.me>
2025-01-04 17:50:08 -0300

    tests

--- apps/spiral/build.ps1
+++ apps/spiral/build.ps1
@@ -15,7 +15,15 @@ $projectName = "spiral"
 
 if (!$SkipPreBuild -and !$SkipFsx) {
     if (!$fast -and !$SkipNotebook) {
+        echo "../../deps ls"
         { ls } | Invoke-Block -Location ../../deps
+        echo "../.. ls"
+        { ls } | Invoke-Block -Location ../..
+        echo "../../workspace ls"
+        { ls } | Invoke-Block -Location ../../workspace
+        echo "../../workspace/target ls"
+        { ls } | Invoke-Block -Location ../../workspace/target
+        echo "../../workspace/target/release ls"
         { ls } | Invoke-Block -Location ../../workspace/target/release
 
         { . ../../deps/polyglot/apps/spiral/dist/Supervisor$(_exe) --execute-command "../../workspace/target/release/spiral$(_exe) dib --path $projectName.dib" } | Invoke-Block -Retries 3
