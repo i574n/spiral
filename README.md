e64a52a43eac439e31e79bb908ef26732f882217
fc1943s <fc1943s@users.noreply.github.com>
2025-02-26 10:44:03 +0000

    deploy: ff212d890b3cfa861e2d73b96fcd73d192f12801

--- README.md
+++ README.md
@@ -6,8 +6,91 @@
 
 ---
 
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+- [Links](#links)
 - [Contributing](#contributing)
 - [License](#license)
+
+## Getting Started
+
+### Installation
+
+Clone the repository:
+
+```powershell
+git clone https://github.com/i574n/spiral.git
+```
+
+Navigate to the directory, initialize dependencies and build the project:
+
+```powershell
+cd spiral
+pwsh scripts/workflow.ps1
+```
+
+## Links
+
+<table>
+
+<tr>
+<td>
+
+Documentation
+
+</td>
+<td>
+
+- Repository file tree (HTML)  
+<https://i574n.github.io/spiral>
+
+</td>
+</tr>
+
+<tr>
+<td>
+
+Samples
+
+</td>
+<td>
+
+- Spiral CLI notebook  
+<https://i574n.github.io/spiral/apps/spiral/spiral.dib.html>
+
+- Spiral compiler notebook  
+<https://i574n.github.io/spiral/apps/compiler/spiral_compiler.dib.html>
+
+- Parsing library  
+<https://i574n.github.io/spiral/lib/spiral/parsing.dib.html>
+
+- Physics notebook from the "Learn Physics with Functional Programming" book  
+<https://i574n.github.io/spiral/lib/spiral/physics.dib.html>
+
+</td>
+</tr>
+
+<tr>
+<td>
+
+Social
+
+</td>
+<td>
+
+- Spiral ChatGPT pitch conversation (en-US)  
+<https://chat.openai.com/share/62ae3405-4d5f-4cff-a7d9-10ed67cc7cf2>
+
+- Spiral ChatGPT pitch conversation (pt-BR)  
+<https://chat.openai.com/share/1ad5a1de-ca7a-4d26-8594-f1901c56281c>
+
+- Forum Post (pt-BR): Pitch: A linguagem de programação Spiral  
+<https://www.tabnews.com.br/i574n/pitch-a-linguagem-de-programacao-spiral-chatgpt>
+
+</td>
+</tr>
+
+</table>
 
 ## Contributing
 
--- lib/spiral/common.py
+++ lib/spiral/common.py
@@ -200,7 +200,7 @@ class US5(Union):
 
 US5_reflection = _expr17
 
-def _expr20() -> TypeInfo:
+def _expr18() -> TypeInfo:
     return union_type("Common.US6", [], US6, lambda: [[], []])
 
 
@@ -216,9 +216,9 @@ class US6(Union):
         return ["US6_0", "US6_1"]
 
 
-US6_reflection = _expr20
+US6_reflection = _expr18
 
-def _expr23() -> TypeInfo:
+def _expr19() -> TypeInfo:
     return union_type("Common.US7", [], US7, lambda: [[("f0_0", US6_reflection())], []])
 
 
@@ -234,7 +234,7 @@ class US7(Union):
         return ["US7_0", "US7_1"]
 
 
-US7_reflection = _expr23
+US7_reflection = _expr19
 
 def method3(v0_1: str) -> str:
     return v0_1
@@ -249,10 +249,10 @@ def closure1(unit_var: None, v0_1: str) -> US5:
 
 
 def method5(__unit: None=None) -> Callable[[str], US5]:
-    def _arrow26(v: str) -> US5:
+    def _arrow22(v: str) -> US5:
         return closure1(None, v)
 
-    return _arrow26
+    return _arrow22
 
 
 def method2(v0_1: str) -> str:
@@ -280,7 +280,7 @@ def method2(v0_1: str) -> str:
 def method1(__unit: None=None) -> tuple[US1, US2]:
     v1_1: str = method2("TRACE_LEVEL")
     v6: US1 = US1(0, US0(0)) if ("Verbose" == v1_1) else US1(1)
-    def _arrow41(__unit: None=None) -> US1:
+    def _arrow39(__unit: None=None) -> US1:
         v13: US1 = US1(0, US0(1)) if ("Debug" == v1_1) else US1(1)
         if v13.tag == 0:
             return US1(0, v13.fields[0])
@@ -302,7 +302,7 @@ def method1(__unit: None=None) -> tuple[US1, US2]:
 
 
 
-    return (US1(0, v6.fields[0]) if (v6.tag == 0) else _arrow41(), US2(1) if (method2("AUTOMATION") != "True") else US2(0, from_value(ticks_1(now()), False)))
+    return (US1(0, v6.fields[0]) if (v6.tag == 0) else _arrow39(), US2(1) if (method2("AUTOMATION") != "True") else US2(0, from_value(ticks_1(now()), False)))
 
 
 def closure2(unit_var: None, v0_1: str) -> None:
@@ -337,11 +337,11 @@ def method7(v0_1: Callable[[], None]) -> Callable[[], None]:
 
 def closure3(unit_var: None, v0_1: Callable[[], None]) -> IDisposable:
     v10: Callable[[], None] = method7(v0_1)
-    class ObjectExpr43(IDisposable):
+    class ObjectExpr44(IDisposable):
         def Dispose(self, __unit: None=None, unit_var: Any=unit_var, v0_1: Any=v0_1) -> None:
             v10(None)
 
-    return ObjectExpr43()
+    return ObjectExpr44()
 
 
 def closure6(unit_var: None, v0_1: US6) -> US7:
@@ -368,17 +368,17 @@ def method9(v0_1: US0) -> bool:
         return False
 
     else: 
-        class ObjectExpr63:
+        class ObjectExpr58:
             @property
             def Compare(self) -> Callable[[US0, US0], int]:
                 return compare
 
-        class ObjectExpr66:
+        class ObjectExpr61:
             @property
             def Compare(self) -> Callable[[US0, US0], int]:
                 return compare
 
-        return find(v0_1, of_seq(to_enumerable([(US0(0), 0), (US0(1), 1), (US0(2), 2), (US0(3), 3), (US0(4), 4)]), ObjectExpr63())) >= find(v35, of_seq(to_enumerable([(US0(0), 0), (US0(1), 1), (US0(2), 2), (US0(3), 3), (US0(4), 4)]), ObjectExpr66()))
+        return find(v0_1, of_seq(to_enumerable([(US0(0), 0), (US0(1), 1), (US0(2), 2), (US0(3), 3), (US0(4), 4)]), ObjectExpr58())) >= find(v35, of_seq(to_enumerable([(US0(0), 0), (US0(1), 1), (US0(2), 2), (US0(3), 3), (US0(4), 4)]), ObjectExpr61()))
 
 
 
@@ -387,10 +387,10 @@ def closure10(unit_var: None, v0_1: int64) -> US2:
 
 
 def method11(__unit: None=None) -> Callable[[int64], US2]:
-    def _arrow68(v: int64) -> US2:
+    def _arrow64(v: int64) -> US2:
         return closure10(None, v)
 
-    return _arrow68
+    return _arrow64
 
 
 def method12(__unit: None=None) -> str:
