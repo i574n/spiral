29f0072178bfaffd41fd6d36fc1fd15159ac9483
fc1943s <fc1943s@users.noreply.github.com>
2025-10-29 03:09:11 +0000

    deploy: 2bf5f40521dc45d2250a88b13f8fb40a46c06899

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -6,9 +6,13 @@ Set-Location $ScriptDir
 $ErrorActionPreference = "Stop"
 
 
+$url = git ls-remote --get-url
+$owner = ($url -split '/' | Select-Object -Last 2 | Select-Object -First 1) -replace '\.git$', '' ?? $env:GITHUB_REPOSITORY_OWNER
+Write-Output "init.ps1 / url: $url / owner: $owner"
+
 if (!$fast) {
     Set-Location (New-Item -ItemType Directory -Path "../.." -Force)
-    git clone --recurse-submodules https://github.com/i574n/polyglot.git # --branch gh-pages
+    git clone --recurse-submodules https://github.com/$owner/polyglot.git # --branch gh-pages
     Set-Location polyglot
     git pull
     Set-Location $ScriptDir
